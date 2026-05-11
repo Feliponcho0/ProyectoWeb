bb9c8b599675e365d8eed5b4327340110557e3ec
Felipe <castillofelipejesus@gmail.com>
2026-05-10 18:38:13 -0700

    cambio en validarLogin.php

--- validations/validarLogin.php
+++ validations/validarLogin.php
@@ -1,33 +1,28 @@
-<?php
-    require_once 'check.php';
+?php
+    require_once 'check.php'; 
     require_once 'conection.php';
 
     $nombre_usuario = $_POST['nombre_usuario'] ?? '';
     $contra = $_POST['password'] ?? '';
     
-    $query = "SELECT rol FROM usuarios WHERE nombre_usuario = ? AND password = ?";
+    $query = "SELECT id_usuario, rol, tiendas_id FROM usuarios WHERE nombre_usuario = ? AND password = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("ss", $nombre_usuario, $contra);
     $stmt->execute();
     $resultado = $stmt->get_result();
-    //$fila = $resultado->fetch_assoc();
-    // $rol = $fila['rol'];
-    // echo $rol;
 
     if ($resultado->num_rows > 0) {
-
         $fila = $resultado->fetch_assoc();
-        $rol = $fila['rol'];
+        
+        $_SESSION['id_usuario'] = $fila['id_usuario'];
+        $_SESSION['rol'] = $fila['rol']; // guardamos aqui el rol
+        $_SESSION['tiendas_id'] = $fila['tiendas_id']; 
 
-        if ($rol === "Administrador") {
-            header('Location: ../pages/dashboard.php');
-        } else if ($rol === "Gerente") {
-            
-            header('Location: ../pages/dashboard.php');
-        } else if ($rol === "Cajero") {
-            header('Location: ../pages/dashboard.php');
-        }
+        header('Location: ../pages/dashboard.php');
+        exit();
+
     } else {
         header('Location: ../index.php?error=1');
+        exit();
     }
 ?>
