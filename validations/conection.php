46fb4b9ec280b485c0b82f025bd591991539a792
Elsami7 <l233110182@cdconstitucion.tecnm.mx>
2026-06-03 20:21:51 -0700

    Pruebas y validaciones

--- validations/conection.php
+++ validations/conection.php
@@ -1,4 +1,5 @@
 <?php
+    /*
     $servername = "bdzgbhzoece2npuq85jz-mysql.services.clever-cloud.com";
     $username = "u73962dkev2xk0n9";
     $password = "wUlj5spC069pdX720lpm";
@@ -8,6 +9,19 @@
     // $username = "if0_42064577";
     // $password = "1644162176";
     // $database = "if0_42064577_core";
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn) {
+        die("Error de conexión: " . mysqli_connect_error());
+    }
+
+    */
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "1644162176";
+    $database = "mydb2";
 
     $conn = mysqli_connect($servername, $username, $password, $database);
 
