b7822eb6395512e46ad1a200f96002b21233f858
Felipe <castillofelipejesus@gmail.com>
2026-06-08 20:15:39 -0700

    Terminado

--- views/punto_venta_view.php
+++ views/punto_venta_view.php
@@ -33,7 +33,7 @@
             <span class="input-group-text">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
             </span>
-            <input type="text" id="busqueda_producto" class="form-control border-primary" placeholder="Buscar producto...">
+            <input type="text" id="busqueda_producto" class="form-control border-primary" placeholder="Buscar producto..." <?php echo (!$puede_vender) ? 'disabled' : ''; ?> >
         </div>
     </div>
 
@@ -82,7 +82,6 @@
                     <input type="text" id="cambio" class="form-control" readonly>
                 </div>
             </div>
-          
             <div class="modal-footer">
                 <button class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                 <button id="confirmar_pago" class="btn btn-success">Confirmar</button>
